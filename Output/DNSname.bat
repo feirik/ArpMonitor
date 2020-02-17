78afa06525a00b0ef84150d1d7a9378c5b04e1fd
Eirik <feirik@gmail.com>
2020-02-17 22:15:29 +0100

    Updated comment

--- Output/DNSname.bat
+++ Output/DNSname.bat
@@ -6,7 +6,7 @@ set IPaddress=%1
 :: Default name output if no 'Name:' is found in nslookup
 set DNSname=Unknown
 
-:: If a DNS name is found, store in variable
+:: If a DNS name is found, store in variable, filter out cerr output from no name found
 for /f "tokens=2 delims=:" %%f in ('nslookup %IPaddress% 2^>^&1 ^| find /i "Name"') do set DNSname=%%f
 
 :: Stripping spaces from the token variable
